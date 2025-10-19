e8f9afea2b252e263187e4d1e8b2c99c46792350
TinyRedBird <149476621+TinyRedBird@users.noreply.github.com>
2025-10-19 19:24:10 +0800

    评论删除功能

--- MyApplication9/entry/src/main/ets/mode/Users.ts
+++ MyApplication9/entry/src/main/ets/mode/Users.ts
@@ -2,9 +2,11 @@ export class Users {
   id: number;
   account: string;
   password: string;
-  constructor(id:number,account: string, password: string) {
+  avatar: string = '';
+  constructor(id:number,account: string, password: string, avatar: string) {
     this.id = id;
     this.account = account;
     this.password = password;
+    this.avatar = avatar;
   }
 }
